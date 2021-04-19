1f8eb59923416d8a621740dc8ed8d820e341c7e3
laiagc892 <laiagc892@gmail.com>
2021-04-19 14:40:59 +0200

    update ex

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ var app = express();
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+//Si en las peticiones del cliente, encuentras información codificada de este tipo (urlencoded); parséamela y deposita la información en req.body
+app.use(express.urlencoded({ extended: false }));
 
 // use res.render to load up an ejs view file
 
 // index page 
 app.get('/', function(req, res) {
-
     var tagline = "No programming concept is complete without a cute animal mascot.";
-
     res.render('pages/index', {
         mascots: mascots,
         tagline: tagline
@@ -28,6 +28,27 @@ app.get('/', function(req, res) {
 // about page
 app.get('/about', function(req, res) {
     res.render('pages/about');
+});
+
+// add-mascot page
+app.get('/add-mascot', function(req, res) {
+    var tagline = "Add your new mascot here!";
+    res.render('pages/form', {
+        tagline: tagline
+    });
+});
+
+// post add-mascot
+app.post('/add-mascot', function(req, res) {
+    console.log(req.body);
+    let newMascot = {
+        name: req.body.name,
+        organization: req.body.organization,
+        birth_year: req.body.year
+    };
+    mascots.push(newMascot);
+    
+    res.redirect('/');
 });
 
 app.listen(8080);
